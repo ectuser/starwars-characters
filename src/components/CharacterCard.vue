868cdde349da7560364ed6aa804156d6d22ffd46
ectuser <m.ectuser@gmail.com>
2020-11-23 21:30:02 +0700

    Created Card component and added styles for it

---
+++ src/components/CharacterCard.vue
@@ -0,0 +1,39 @@
+<template>
+  <md-card>
+      <md-card-header>
+        <md-card-header-text>
+          <div class="md-title">{{name}}</div>
+          <div class="md-subhead">Subtitle here</div>
+        </md-card-header-text>
+        <md-card-media>
+          <img :src="imgUrl" alt="People">
+        </md-card-media>
+      </md-card-header>
+
+      <md-card-actions>
+        <md-button class="md-icon-button">
+          <md-icon>favorite</md-icon>
+        </md-button>
+      </md-card-actions>
+    </md-card>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class CharacterCard extends Vue {
+  @Prop() private name!: string;
+  @Prop() private imgUrl!: string;
+}
+</script>
+
+<style>
+  .md-card{
+    width: 80%;
+    margin: 0 auto;
+    margin-bottom: 20px;
+  }
+  /* .md-card img{
+    height: 400px;
+  } */
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Hello world"/>
+    <CharacterCard name="123" imgUrl="https://starwars-visualguide.com/assets/img/characters/1.jpg" />
+    <CharacterCard name="123" imgUrl="https://starwars-visualguide.com/assets/img/characters/2.jpg" />
+    <CharacterCard name="123" imgUrl="https://starwars-visualguide.com/assets/img/characters/3.jpg" />
+    <CharacterCard name="123" imgUrl="https://starwars-visualguide.com/assets/img/characters/4.jpg" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CharacterCard from '@/components/CharacterCard'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    CharacterCard
   }
 }
 </script>
+
+<style>
+  .home{
+    margin-top: 10px;
+    display: flex;
+    flex-direction: column;
+  }
+</style>
