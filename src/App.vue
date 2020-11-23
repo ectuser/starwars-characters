822449797dbb373ca61e5018af2ed5c9df0a06cb
ectuser <m.ectuser@gmail.com>
2020-11-23 21:02:45 +0700

    Added material styles and connected them

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <md-toolbar class="md-accent" md-elevation="1">
+      <h3 class="md-title" style="flex: 1">Star Wars</h3>
+      <md-button to="/">Home</md-button>
+      <md-button to="/favorites">Favorites</md-button>
+    </md-toolbar>
     <router-view/>
   </div>
 </template>
+<script lang="ts">
+import Vue from 'vue'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+Vue.use(VueMaterial)
+export default Vue.extend({
+  
+})
+</script>
