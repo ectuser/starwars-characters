f74fc6526b14111588fa1f05c278e871c38e9022
ectuser <m.ectuser@gmail.com>
2020-11-23 23:48:32 +0700

    Pagination is done

--- src/services/characters/api/characters.ts
+++ src/services/characters/api/characters.ts
@@ -2,8 +2,10 @@ import { apiConfig } from "./api-config";
 import axios from "axios";
 import { AllCharacters } from "@/interfaces/all-characters";
 export const CharactersApiService = {
-  async getAll(): Promise<AllCharacters> {
-    const allData = await axios.get(`${apiConfig.baseUrl}/people/`);
+  async getCharactersFromPage(pageNumber: number): Promise<AllCharacters> {
+    const allData = await axios.get(
+      `${apiConfig.baseUrl}/people/?page=${pageNumber}`
+    );
     if (allData.status !== 200) {
       throw new Error("Can't get data");
     }
