b021647f28fc91f829287290856d3dd3f8cdb3e9
ectuser <m.ectuser@gmail.com>
2020-11-24 15:13:26 +0700

    Last changes

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
 </template>
 <script lang="ts">
 import Vue from 'vue'
+// @ts-ignore
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
